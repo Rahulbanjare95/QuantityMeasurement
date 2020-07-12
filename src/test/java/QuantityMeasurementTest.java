2f9cc799984a7103e3f7d6323a19b80e8a9296ae
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-12 20:52:33 +0530

    Added: Test case to calculate inch to feet conversion

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -1,8 +1,12 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
+
     @Test
     public void givenQuantity_inFeet_ShouldConvert_toInches() {
-
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        double actual = quantityMeasurement.convert(1);
+        Assert.assertEquals(12,actual,0.0);
     }
 }
