2e7ede475182788eef286151970855a94befe07a
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-13 22:19:38 +0530

    Added: Test case checks for reference type of Inch

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -23,36 +23,45 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenReference_SameObject_ShouldReturnTrue() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.FEET,1.0);
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.FEET, 1.0);
         QuantityMeasurement quantityMeasurement1 = quantityMeasurement;
         Assert.assertTrue(quantityMeasurement.equals(quantityMeasurement1));
     }
 
     @Test
     public void givenClassType_IfDifferent_ShouldReturnFalse() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.FEET,1.0);
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.FEET, 1.0);
         String message = "Hello World!";
         Assert.assertFalse(quantityMeasurement.getClass().equals(message.getClass()));
     }
 
     @Test
     public void givenUnitValue_shouldCheck_EqualtiyInEntity_ReturnsTrue() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.FEET,1.0);
-        QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement(Unit.FEET,1.0);
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.FEET, 1.0);
+        QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement(Unit.FEET, 1.0);
         Assert.assertTrue(quantityMeasurement.equals(quantityMeasurement1));
     }
 
     @Test
     public void givenZeroInch_shouldReturnEqual() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.INCH,0.0);
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.INCH, 0.0);
         QuantityMeasurement quantityMeasurement1 = new QuantityMeasurement(Unit.INCH, 0.0);
         Assert.assertEquals(quantityMeasurement,quantityMeasurement1);
     }
 
     @Test
     public void givenNullValue_ShouldReturnFalse() {
-        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.INCH,0.0);
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.INCH, 0.0);
         QuantityMeasurement nullValue = null;
         Assert.assertFalse(quantityMeasurement.equals(nullValue));
     }
+
+    @Test
+    public void givenReference_ForSameObject_ForInch_ShouldReturnTrue() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement(Unit.INCH, 0.0);
+        QuantityMeasurement reference = quantityMeasurement;
+        Assert.assertTrue(quantityMeasurement.equals(reference));
+    }
+
+
 }
