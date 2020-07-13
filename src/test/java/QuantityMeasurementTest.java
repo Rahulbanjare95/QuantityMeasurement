29b921cc64716264ada5791b562788cd3b92fcc7
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-13 08:26:22 +0530

    Refactored: Test cases to throw custom exception

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -4,9 +4,20 @@ import org.junit.Test;
 public class QuantityMeasurementTest {
 
     @Test
-    public void givenQuantity_inFeet_ShouldConvert_toInches() {
+    public void givenQuantity_inFeet_ShouldConvert_toInches() throws QuantityMeasurementException {
         QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
         double actual = quantityMeasurement.convert(1);
         Assert.assertEquals(12,actual,0.0);
+    }
+
+    @Test
+    public void givenQuantity_inNegativeFeet_ShouldThrowException() {
+        QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+        try {
+            double actual = quantityMeasurement.convert(-2);
+            Assert.assertEquals(24,actual,0.0);
+        } catch (QuantityMeasurementException e) {
+            e.getMessage();
+        }
     }
 }
