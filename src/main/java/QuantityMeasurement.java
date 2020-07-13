5ce2263ca78a1e06cbb921fe4d4820e135f6c14b
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-07-13 08:25:29 +0530

    Added: Code to check for negative units

--- src/main/java/QuantityMeasurement.java
+++ src/main/java/QuantityMeasurement.java
@@ -1,6 +1,11 @@
+import javax.lang.model.element.QualifiedNameable;
+
 public class QuantityMeasurement {
 
-    public double convert(int inch) {
+    public double convert(int inch) throws QuantityMeasurementException {
+        if(inch < 0)
+            throw new QuantityMeasurementException("Negative Units Not allowed",
+                    QuantityMeasurementException.ExceptionType.NEGATIVE_UNIT_ERROR);
         return inch*12;
     }
 }
